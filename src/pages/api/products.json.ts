41d42a61371fdf2576765e802308df9391bcade1
Nerea Cassian <nerea@nereacassian.com>
2024-01-23 23:38:54 +0100

    initial commit

---
+++ src/pages/api/products.json.ts
@@ -0,0 +1,6 @@
+import products from '../../data/products.json';
+import type { APIRoute } from 'astro';
+
+export const GET: APIRoute = ({ request }) => {
+  return new Response(JSON.stringify(products))
+}
---
+++ src/pages/api/socials.json.ts
@@ -0,0 +1,6 @@
+import socialsjson from '../../data/socials.json';
+import type { APIRoute } from 'astro';
+
+export const GET: APIRoute = ({ request }) => {
+  return new Response(JSON.stringify(socialsjson))
+}
